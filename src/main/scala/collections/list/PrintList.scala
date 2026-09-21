84a6e037dd27fa5aa8667ae151b74aeac8b23a98
Vasanth Subramanian <vasant.techie@gmail.com>
2026-09-21 11:38:15 +0530

    List collections basics, Printing the List elements

---
+++ src/main/scala/collections/list/PrintList.scala
@@ -0,0 +1,12 @@
+package collections.list
+
+/**
+ * This code just prints the List values.
+ /// @return
+ */
+@main
+def printList(): Unit = {
+  println(List(1, 2, "hello", true, 3.0))
+  println(Nil) //Nil -> Empty List
+  println(List()) // Empty List
+}
