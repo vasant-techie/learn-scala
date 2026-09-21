0c13d3e9060ca58d06080aed941eeb09cf3a3355
Vasanth Subramanian <vasant.techie@gmail.com>
2026-09-21 11:47:32 +0530

    added type specific List

--- src/main/scala/collections/list/PrintList.scala
+++ src/main/scala/collections/list/PrintList.scala
@@ -9,4 +9,6 @@ def printList(): Unit = {
   println(List(1, 2, "hello", true, 3.0))
   println(Nil) //Nil -> Empty List
   println(List()) // Empty List
+  println(List[Boolean](true, false, true)) // Type-specific (Boolean) List
+  println(List[Int](1, 2, 3, 1, 0, -1, -45)) // Type specific (Int) List
 }
